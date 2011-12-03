3da9bec44fbeb17613bd834c9d5b839b3ffabf6d
Ralph <ralph@radds.de>
2011-12-03 23:51:23 +0100

    added setup script an further information about the platform

--- setup.sh
+++ setup.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-rm -rf live && mkdir live
+echo "Removing existing live folder and creating new one"
+if [ -e "cakephp.zip" ]; then
+	rm cakephp.zip
+fi
+
+if [ -d "live" ]; then
+	rm -rf live
+fi
+
+mkdir live
 cd live
-curl --ssl --connect-timeout 10 --get https://github.com/cakephp/cakephp/zipball/master -o cakephp.zip
-unzip cakephp.zip
+
+echo "Downloading current cakePHP version from github.com"
+curl --connect-timeout 10  -o cakephp.zip https://nodeload.github.com/cakephp/cakephp/zipball/master
+
+if [ -e "cakephp.zip" ]; then
+	echo "Unzipping cakePHP and setting-up cakePHP installation"
+	unzip cakephp.zip && mv cakephp.zip ../cakephp.zip
+	mv cakephp-* cake-src
+	mv cake-src/* . && rm -rf cake-src
+else
+	echo "Was not able to download the current cakephp version from github.com"
+	echo "Please try later."
+	return 0
+fi
+
+echo "Setting up e-learning platform"
 rsync -Curz ../src/ .
+
+echo "Done."
+
 
